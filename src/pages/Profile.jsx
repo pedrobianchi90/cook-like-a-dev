deed8a1083f1a5c312a47ef59f851f8309382d11
Mariah M. Rodrigues <mariah.manetti@gmail.com>
2022-04-27 16:16:52 -0300

    profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import Footer from '../components/Footer';
 import profileIcon from '../images/profileIcon.svg';
 
 function Profile() {
+  const history = useHistory();
+
+  const handleFav = () => {
+    history.push('favorite-recipes');
+  };
+
+  const handleDone = () => {
+    history.push('done-recipes');
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  const getEmail = JSON.parse(localStorage.getItem('user'));
+  console.log(getEmail.email);
+
   return (
     <div>
       <header>
@@ -11,6 +30,42 @@ function Profile() {
         </button>
         <h2 data-testid="page-title">Profile</h2>
       </header>
+
+      <section>
+        <p
+          data-testid="profile-email"
+          id="profile-email"
+        >
+          { getEmail.email }
+        </p>
+
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          id="fav-btn"
+          onClick={ handleFav }
+        >
+          Favorite Recipes
+        </button>
+
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          id="done-btn"
+          onClick={ handleDone }
+        >
+          Done Recipes
+        </button>
+
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          id="logout-btn"
+          onClick={ handleLogout }
+        >
+          Logout
+        </button>
+      </section>
       <Footer />
     </div>
   );
