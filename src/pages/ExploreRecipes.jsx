117bedab667c5b0e9a7bedf9cb07f984fd9a10a8
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-04-27 21:12:12 -0300

    Adicionando requisitos 70 a 73

--- src/pages/ExploreRecipes.jsx
+++ src/pages/ExploreRecipes.jsx
@@ -5,6 +5,15 @@ import profileIcon from '../images/profileIcon.svg';
 
 function ExploreRecipes() {
   const location = useLocation();
+
+  const getIdFood = () => async () => {
+    const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    const response = await fetch(url);
+    const data = await response.json();
+    const { idMeal } = data.meals[0];
+    console.log(idMeal);
+    /* return <Link data-testid="product-detail-link" to={ `/explore/foods/${idMeal}` } />; */
+  };
   return (
     <div>
       <header>
@@ -13,10 +22,39 @@ function ExploreRecipes() {
             <img src={ profileIcon } alt="logo-profile" data-testid="profile-top-btn" />
           </button>
         </Link>
-        <h2 data-testid="page-title">
-          {location.pathname === '/explore/drinks' ? 'Explore Drinks' : 'Explore Foods' }
-        </h2>
       </header>
+      {location.pathname === '/explore/foods'
+        ? (
+          <>
+            <h2 data-testid="page-title">Explore Foods</h2>
+            <Link to="/explore/foods/ingredients">
+              <button type="button" data-testid="explore-by-ingredient">
+                By Ingredient
+              </button>
+            </Link>
+            <Link to="/explore/foods/nationalities">
+              <button type="button" data-testid="explore-by-nationality">
+                By Nationality
+              </button>
+            </Link>
+            <button
+              type="button"
+              data-testid="explore-surprise"
+              onClick={ getIdFood() }
+            >
+              Surprise me!
+            </button>
+          </>)
+        : (
+          <>
+            <h2 data-testid="page-title">Explore Drinks</h2>
+            <Link to="/explore/drinks/ingredients">
+              <button type="button" data-testid="explore-by-ingredient">
+                By Ingredient
+              </button>
+            </Link>
+            <button type="button" data-testid="explore-surprise">Surprise me!</button>
+          </>)}
       <Footer />
     </div>
   );
