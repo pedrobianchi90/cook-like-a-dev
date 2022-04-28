487ba7efdc1b3a6922d13a591b9e82998db75fc0
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-04-28 17:32:36 -0300

    Iniciando Página de Details

---
+++ src/services/fetchRecipeById.js
@@ -0,0 +1,14 @@
+const endpointFood = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+const endpointDrink = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+export const foodById = async (id) => {
+  const response = await fetch(`${endpointFood}${id}`);
+  const data = await response.json();
+  return data.meals;
+};
+
+export const drinkById = async (id) => {
+  const response = await fetch(`${endpointDrink}${id}`);
+  const data = await response.json();
+  return data.drinks;
+};
