fbde59ea54325819c2e43121b13e7b0eae4d6475
Pedro Bianchi <pedroedm@gmail.com>
2022-05-03 15:42:20 -0300

    começando tela de receita favoritas

--- src/pages/FavoriteRecipe.jsx
+++ src/pages/FavoriteRecipe.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+
+const copy = require('clipboard-copy');
 
 function FavoriteRecipe() {
+  const [alert, setAlert] = useState(false);
+  const [filter, setFilter] = useState('');
+
+  const copyToClipboard = (url) => {
+    copy(url);
+    setAlert(!alert);
+  };
+
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
   return (
     <div>
       <header>
@@ -12,6 +25,91 @@ function FavoriteRecipe() {
           </button>
         </Link>
         <h2 data-testid="page-title">Favorite Recipes</h2>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => setFilter('') }
+        >
+          All
+
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          onClick={ () => setFilter('Food') }
+        >
+          Food
+
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => setFilter('Drink') }
+        >
+          Drinks
+
+        </button>
+        {doneRecipes.filter(({ type }) => type.includes(filter)).map((recipe, index) => (
+          <div key={ recipe.id }>
+            { recipe.type === 'Food'
+              ? (
+                <>
+                  <p data-testid={ `${index}-horizontal-top-text` }>
+                    {' '}
+                    {`${recipe.nationality} - ${recipe.category}`}
+                  </p>
+                  <Link to={ `/foods/${recipe.id}` }>
+                    <img
+                      src={ recipe.image }
+                      alt=""
+                      data-testid={ `${index}-horizontal-image` }
+                    />
+                    <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+                  </Link>
+                  <button
+                    type="button"
+                    data-testid={ `${index}-horizontal-share-btn` }
+                    onClick={ () => copyToClipboard(`http://localhost:3000/foods/${recipe.id}`) }
+                  >
+                    <img src={ shareIcon } alt="logo-share" />
+                  </button>
+                  <button
+                    type="button"
+                  >
+                    <img src={ blackHeartIcon } alt="logo-share" />
+                  </button>
+
+                </>)
+              : (
+                <>
+                  <p data-testid={ `${index}-horizontal-top-text` }>
+                    {recipe.alcoholicOrNot}
+                  </p>
+                  <Link to={ `/drinks/${recipe.id}` }>
+                    <img
+                      src={ recipe.image }
+                      alt=""
+                      data-testid={ `${index}-horizontal-image` }
+                    />
+                    <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+                  </Link>
+                  <button
+                    type="button"
+                    data-testid={ `${index}-horizontal-share-btn` }
+                    onClick={ () => copyToClipboard(`http://localhost:3000/drinks/${recipe.id}`) }
+                  >
+                    <img src={ shareIcon } alt="logo-share" />
+                  </button>
+                  <button
+                    type="button"
+                  >
+                    <img src={ blackHeartIcon } alt="logo-share" />
+                  </button>
+
+                </>)}
+            { alert && <span>Link copied!</span> }
+          </div>
+        )) }
       </header>
     </div>
   );
